c58c16055682fe88806885235f22ee66fa3e64c2
Carla <152617155+Cl1493@users.noreply.github.com>
2024-06-29 18:35:20 +0200

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Todos from "./Todos";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	
+	return (
+		
+		<div className="text-center">
+			<Todos />
+		</div>
+		
+	);
+};
+
+export default Home;
